d19a4db7c27e96f1f4bc5cc8b141cd63ee62bc66
wac <centeio.wagner@gmail.com>
2019-10-24 21:25:11 -0100

    part 3 project submited for check

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -15,7 +15,5 @@ export class ProductDescriptionComponent implements OnInit {
     this._productService
       .getAlbum(1)
       .subscribe(response => (this.albumInfo = response));
-
-    debugger;
   }
 }
